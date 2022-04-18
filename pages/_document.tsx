2ed7005a5ea94ffc257bed83b39107fd0e5c0a63
Vittitow <corey.vittitow@gmail.com>
2022-04-18 01:18:38 +0000

    feat: add baseurl to remove relative path error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ class CustomDocument extends Document {
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
+    const protocol = ctx.req?.headers['x-forwarded-proto'] || 'http'
+    const baseUrl = ctx.req ? `${protocol}://${ctx.req.headers.host}` : ''
+    
+    process.env.BASE_URL = baseUrl;
 
     return initialProps;
   }
--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -5,9 +5,9 @@ import getQuotes from '@libs/quote-api';
 export default async function middleware(
   request: NextRequest
 ) {
-
+  
   if(request.nextUrl.pathname === '/api/status') {
-    return new Response(null, { status: 200, statusText: 'OK' });
+    return new Response('Hello World!');
   }
 
   if(request.nextUrl.pathname === '/api/quotes' && request.method === 'POST') {
