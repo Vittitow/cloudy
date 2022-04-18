defe06846e7729aa91ede36a0528f3e0c5b8b8a9
Vittitow <corey.vittitow@gmail.com>
2022-04-18 02:41:39 +0000

    fix(worker): request body null in middleware

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,26 +1,25 @@
-import type { NextRequest } from 'next/server';
-import getQuotes from '@libs/quote-api';
+import type { NextRequest } from "next/server";
+import getQuotes from "@libs/quote-api";
 
-
-export default async function middleware(
-  request: NextRequest
-) {
-
-  if(request.nextUrl.pathname === '/api/status') {
-    return new Response('Hello World!');
+export default async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/api/status") {
+    return new Response("Hello World!");
   }
 
-  if(request.nextUrl.pathname === '/api/quotes' && request.method === 'POST') {
-    console.log('/api/quotes');
-    const data = await request.json();
-    console.log(data);
-    const quotes = await getQuotes(data);
-    console.log(quotes);
+  if (request.nextUrl.pathname === "/api/quotes") {
+    const {
+      nextUrl: { search },
+    } = request;
+
+    const params: any = Object.fromEntries(
+      new URLSearchParams(search).entries()
+    );
+
+    const quotes = await getQuotes(params);
 
     return new Response(JSON.stringify(quotes), {
       status: 200,
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
   }
-
 }
--- src/components/recommendations/recommendation.ts
+++ src/components/recommendations/recommendation.ts
@@ -1,13 +1,17 @@
-import { QuoteRequest } from '@libs/quote-api';
+import { QuoteRequest } from "@libs/quote-api";
 
 export async function getQuotes(request: QuoteRequest) {
   const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
     body: JSON.stringify(request),
   };
 
-  const response = await fetch('/api/quotes', options);
+  const query: string = Object.keys(request)
+    .map((key) => key + "=" + request[key])
+    .join("&");
+    
+  const response = await fetch(`/api/quotes?${query}`, options);
 
   return await response.json();
 }
